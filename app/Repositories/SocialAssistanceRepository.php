bd674fbc6bc8c15ab7e117fb9759a3bc83a4a090
ciyek <aripurnomo606@gmail.com>
2025-07-16 14:03:12 +0700

    feat: implement socialassistance api

---
+++ app/Repositories/SocialAssistanceRepository.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interface\SocialAssistanceRepositoryInterface;
+use App\Models\SocialAssistance;
+use Exception;
+use Illuminate\Support\Facades\DB;
+
+class SocialAssistanceRepository implements SocialAssistanceRepositoryInterface
+{
+  public function getAll(
+    ?string $search,
+    ?int $limit,
+    bool $execute
+  ) {
+    $query = SocialAssistance::where(function ($query) use ($search) {
+      if ($search) {
+        $query->search($search);
+      }
+    });
+
+    if ($limit) {
+      $query->take($limit);
+    }
+
+    if ($execute) {
+      return $query->get();
+    }
+
+    return $query;
+  }
+
+  public function getAllPaginated(
+    ?string $search,
+    ?int $rowPerPage
+  ) {
+    $query = $this->getAll(
+      $search,
+      $rowPerPage,
+      false
+    );
+    
+    return $query->paginate($rowPerPage);
+  }
+
+  public function getById(string $id)
+  {
+    $query = SocialAssistance::where('id', $id);
+
+    return $query->first();
+  }
+
+  public function create(
+    array $data
+  ) {
+    DB::beginTransaction();
+
+    try{
+      $socialAssistance = new SocialAssistance;
+      $socialAssistance->thumbnail = $data['thumbnail']->store('assets/social-assistance', 'public');
+      $socialAssistance->name = $data['name'];
+      $socialAssistance->category = $data['category'];
+      $socialAssistance->amount = $data['amount'];
+      $socialAssistance->provider = $data['provider'];
+      $socialAssistance->description = $data['description'];
+      $socialAssistance->is_available = $data['is_available'];
+      $socialAssistance->save();
+
+      DB::commit();
+
+      return $socialAssistance;
+    }catch (\Exception $e) {
+      DB::rollBack();
+      throw new Exception($e->getMessage());
+    }
+  }
+
+  public function update(string $id, array $data)
+  {
+    DB::beginTransaction();
+
+    try{
+      $socialAssistance = SocialAssistance::find($id);
+
+      if (isset($data['thumbnail'])) {
+        $socialAssistance->thumbnail = $data['thumbnail']->store('assets/social-assistance', 'public');
+      }
+
+      $socialAssistance->name = $data['name'];
+      $socialAssistance->category = $data['category'];
+      $socialAssistance->amount = $data['amount'];
+      $socialAssistance->provider = $data['provider'];
+      $socialAssistance->description = $data['description'];
+      $socialAssistance->is_available = $data['is_available'];
+      $socialAssistance->save();
+
+      DB::commit();
+
+      return $socialAssistance;
+    }catch (\Exception $e) {
+      DB::rollBack();
+      throw new Exception($e->getMessage());
+    }
+  }
+
+  public function destroy(string $id)
+  {
+    DB::beginTransaction();
+
+    try {
+      $socialAssistance = SocialAssistance::find($id);
+
+      if (!$socialAssistance) {
+        throw new Exception('Data Bantuan Sosial Tidak Ditemukan');
+      }
+
+      $socialAssistance->delete();
+
+      DB::commit();
+      return true;
+    } catch (\Exception $e) {
+      DB::rollBack();
+      throw new Exception($e->getMessage());
+    }
+  }
+}
